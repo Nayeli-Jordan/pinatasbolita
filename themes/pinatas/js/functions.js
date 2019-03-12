2853ff5e4422bdbaadf7dc690ce74919f0a2c59a
Nayeli Jordan <[nayeli@queonda.com.mx]>
2019-03-12 17:54:00 -0600

    first commit

---
+++ themes/pinatas/js/functions.js
@@ -0,0 +1,59 @@
+var $=jQuery.noConflict();
+ 
+(function($){
+	"use strict";
+	$(function(){
+ 
+		/*------------------------------------*\
+			#GLOBAL
+		\*------------------------------------*/
+
+		$(document).ready(function() {
+			//footerBottom();
+		});
+ 
+		$(window).on('resize', function(){
+			//footerBottom();
+		});
+ 
+		$(document).scroll(function() {
+
+		});
+ 
+		// if( parseInt( isHome ) ){
+
+		// } 
+
+		// if( parseInt( isSingular ) ){
+
+		// } 
+
+		// Modal
+		$(".open-modal").click(function() {
+			var idModal = $(this).attr('id');
+			$('#modal-' + idModal).show();
+			$('body').addClass('overflow-hide');
+		});
+		$(".close-modal, .exit-modal").click(function() {
+			$('.modal').hide();
+			$('body').removeClass('overflow-hide');
+		});
+
+	});
+})(jQuery);
+ 
+/**
+ * Fija el footer abajo
+ */
+/*
+function footerBottom(){
+	var alturaFooter = getFooterHeight();
+	$('.main-body').css('padding-bottom', alturaFooter );
+}
+function getHeaderHeight(){
+	return $('.js-header').outerHeight();
+}// getHeaderHeight
+function getFooterHeight(){
+	return $('footer').outerHeight();
+}// getFooterHeight
+*/
